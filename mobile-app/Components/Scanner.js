75d6d29cd47233018745e490b4b2ce6557e353f4
Archit <architojha420@gmail.com>
2024-01-21 02:10:53 +0530

    butn

--- mobile-app/Components/Scanner.js
+++ mobile-app/Components/Scanner.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity,Dimensions } from "react-native";
 import { Camera } from "expo-camera";
 import { useFocusEffect } from "@react-navigation/native";
 import axios from "axios";
@@ -8,6 +8,7 @@ const Scanner = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const cameraRef = useRef(null);
+  const { width, height } = Dimensions.get("screen");
 
   const setupCamera = async () => {
     const { status } = await Camera.requestPermissionsAsync();
@@ -42,43 +43,47 @@ const Scanner = () => {
 
   return (
     <View style={{ flex: 1 }}>
-      <Camera
-        style={{ flex: 1 }}
-        type={type}
-        ref={cameraRef}
+    <Camera style={{ flex: 1, zIndex: 2 }} type={type} ref={cameraRef}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "transparent",
+          flexDirection: "row",
+          justifyContent: "space-between",  // Align items at the ends of the container
+          paddingHorizontal: 15,  // Add padding for the flip button
+          paddingTop: 15,  // Add padding for the flip button
+        }}
       >
-        <View
+        <TouchableOpacity
           style={{
-            flex: 1,
-            backgroundColor: "transparent",
-            flexDirection: "row",
+            alignSelf: "flex-start",  // Align to the top left
+          }}
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
           }}
         >
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignSelf: "flex-end",
-              alignItems: "center",
-            }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}
-          >
-            <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
-              Flip
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+          <Text style={{ fontSize: 18, color: "white" }}>Flip</Text>
+        </TouchableOpacity>
+      </View>
+    </Camera>
 
-      <TouchableOpacity onPress={handleButtonClick}>
-        <Text>Click Picture</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity
+      style={{
+        justifyContent: "center",  // Center content vertically
+        alignItems: "center",
+        backgroundColor: 'transparent',
+        position: 'absolute',
+        zIndex: 10,
+      }}
+      onPress={handleButtonClick}
+    >
+      <Text style={{ fontSize: 20, color: 'black', top: height*0.8, backgroundColor: 'white', padding: 13, borderRadius: 30, fontWeight: 'bold', left:width*0.34 }}>Scan Menu</Text>
+    </TouchableOpacity>
+  </View>
   );
 };
 
--- mobile-app/Screens/ocrScreen.js
+++ mobile-app/Screens/ocrScreen.js
@@ -1,15 +1,91 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Dimensions } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-
 const OcrScreen = () => {
-  const navigation=useNavigation();
+  const navigation = useNavigation();
+  const { width, height } = Dimensions.get("screen");
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <TouchableOpacity onPress={()=>navigation.navigate('ScannerScreen')}>
-        <Text>OPEN CAMERA</Text>
+    <View style={{ marginHorizontal: 30, marginTop: 15 }}>
+      <View style={{ marginVertical: 20, marginTop: 40 }}>
+        <Text
+          style={{
+            fontSize: 32,
+            color: "black",
+            fontWeight: "bold",
+          }}
+        >
+          Scan menu from Resturants
+        </Text>
+      </View>
+      <View style={{ width: 0.8 * width, marginBottom: 30 }}>
+        <Text style={{ fontSize: 18, fontWeight: "400", color: "gray" }}>
+          Take a pic of the menu, and our tool shows how healthy your meal is.
+          Easy eats, no confusion.
+        </Text>
+      </View>
+      <View style={{ width: 0.8 * width, marginBottom: 30 }}>
+        <Text style={{ fontSize: 25, fontWeight: "500", marginBottom: 15 }}>
+          Steps:{" "}
+        </Text>
+        <Text
+          style={{
+            fontSize: 15,
+            fontWeight: "400",
+            color: "gray",
+            marginBottom: 5,
+          }}
+        >
+          1. Click on the Open Camera Button
+        </Text>
+        <Text
+          style={{
+            fontSize: 15,
+            fontWeight: "400",
+            color: "gray",
+            marginBottom: 5,
+          }}
+        >
+          2. Take a quick photo of the menu
+        </Text>
+        <Text
+          style={{
+            fontSize: 15,
+            fontWeight: "400",
+            color: "gray",
+            marginBottom: 5,
+          }}
+        >
+          3. Let our OCR magic do its thing
+        </Text>
+        <Text
+          style={{
+            fontSize: 15,
+            fontWeight: "400",
+            color: "gray",
+            marginBottom: 5,
+          }}
+        >
+          5. Instantly see how healthy each dish is, making it easy to choose
+          wisely.
+        </Text>
+      </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("ScannerScreen")}
+        style={{
+          alignItems: "center",
+          marginTop: height * 0.05,
+          backgroundColor: "#fb9c32",
+          padding: 20,
+          marginHorizontal: width * 0.2,
+          borderRadius: 25,
+          elevation: 3,
+        }}
+      >
+        <Text style={{ color: "white", fontWeight: "bold", fontSize: 18 }}>
+          Scan Menu
+        </Text>
       </TouchableOpacity>
     </View>
   );
