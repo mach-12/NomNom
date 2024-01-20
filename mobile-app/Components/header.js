f3b27ed64d9fdff604f957cbb6b3f4f4d7dd730c
Archit <architojha420@gmail.com>
2024-01-21 03:38:53 +0530

    restructured the navigation

--- mobile-app/Components/header.js
+++ mobile-app/Components/header.js
@@ -1,39 +1,40 @@
-import { View, Text, Image, Dimensions } from "react-native";
-import React,{useState} from "react";
+import { View, Text, Image, Dimensions, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native";
 import useAuthStore from "../Components/authStore.js";
 import { FIREBASE_DB } from "../FirebaseConfig.js";
-import {doc,setDoc,updateDoc,getDoc, arrayUnion} from 'firebase/firestore';
-import useUidStore from '../Components/uidStore.js';
+import { doc, setDoc, updateDoc, getDoc, arrayUnion } from "firebase/firestore";
+import useUidStore from "../Components/uidStore.js";
+// import AccountScreen from "../Screens/accountScreen.js";
+import {useNavigation} from '@react-navigation/native'
 
 const { width, height } = Dimensions.get("window");
 
 export default function Header() {
-
-  const authToken=useAuthStore((state)=>state.authToken); 
-  const uid=useUidStore((state)=>state.uid);
-  const [name,setName]=useState();
-
+  const authToken = useAuthStore((state) => state.authToken);
+  const uid = useUidStore((state) => state.uid);
+  const [name, setName] = useState();
+  const navigation=useNavigation();
 
   const retrieve_data = async () => {
-    const docRef = doc(FIREBASE_DB, 'users', uid);
-    
+    const docRef = doc(FIREBASE_DB, "users", uid);
+
     try {
       const docSnap = await getDoc(docRef);
-  
+
       if (docSnap.exists()) {
-        user_data=docSnap.data();
+        user_data = docSnap.data();
         // console.log(user_data.name);
         setName(user_data.name);
       } else {
-        console.log('No such document');
+        console.log("No such document");
       }
     } catch (error) {
-      console.error('Error retrieving data:', error);
+      console.error("Error retrieving data:", error);
     }
-  }
+  };
 
-  retrieve_data()
+  retrieve_data();
 
   return (
     <SafeAreaView className={`flex-row  justify-between mx-10 mt-16`}>
@@ -47,12 +48,19 @@ export default function Header() {
             {name}
           </Text>
         </View>
-        <Text style={{color: "#525252"}}>UI designer & Cook</Text>
+        <Text style={{ color: "#525252" }}>UI designer & Cook</Text>
       </View>
-      <Image
-        source={require("../Assets/person3.png")}
-        style={{ width: width * 0.106, height: height * 0.051, borderRadius: 50 }}
-      />
+
+      <TouchableOpacity onPress={()=>navigation.navigate('AccountScreen')}>
+        <Image
+          source={require("../Assets/person3.png")}
+          style={{
+            width: width * 0.106,
+            height: height * 0.051,
+            borderRadius: 50,
+          }}
+        />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
---
+++ mobile-app/Screens/journalScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const JournalScreen = () => {
+  return (
+    <View>
+      <Text>JournalScreen</Text>
+    </View>
+  )
+}
+
+export default JournalScreen
